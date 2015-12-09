38b1cec7cbeca4b4346a39006c1a13848491327c
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2015-12-09 12:07:59 +0000

    Update effector command to support optional argument 'effector name'.

--- commands/effector.go
+++ commands/effector.go
@@ -10,26 +10,27 @@ import (
 	"github.com/brooklyncentral/brooklyn-cli/scope"
 )
 
-type Effectors struct {
+type Effector struct {
 	network *net.Network
 }
 
-func NewEffectors(network *net.Network) (cmd *Effectors) {
-	cmd = new(Effectors)
+func NewEffector(network *net.Network) (cmd *Effector) {
+	cmd = new(Effector)
 	cmd.network = network
 	return
 }
 
-func (cmd *Effectors) Metadata() command_metadata.CommandMetadata {
+func (cmd *Effector) Metadata() command_metadata.CommandMetadata {
 	return command_metadata.CommandMetadata{
-		Name:        "effectors",
-		Description: "Show the list of effectors for an application and entity",
-		Usage:       "BROOKLYN_NAME [ SCOPE ] effectors",
+		Name:        "effector",
+		Description: "Show the effectors for an application or entity",
+		Usage:       "BROOKLYN_NAME [ SCOPE ] effector [ NAME ]",
 		Flags:       []cli.Flag{},
 	}
 }
 
-func (cmd *Effectors) Run(scope scope.Scope, c *cli.Context) {
+func (cmd *Effector) Run(scope scope.Scope, c *cli.Context) {
+
 	effectors := entity_effectors.EffectorList(cmd.network, scope.Application, scope.Entity)
 	table := terminal.NewTable([]string{"Name", "Description", "Parameters"})
 	for _, effector := range effectors {
@@ -37,7 +38,9 @@ func (cmd *Effectors) Run(scope scope.Scope, c *cli.Context) {
 		for _, parameter := range effector.Parameters {
 			parameters = append(parameters, parameter.Name)
 		}
-		table.Add(effector.Name, effector.Description, strings.Join(parameters, ","))
+        if !c.Args().Present() || c.Args().First() == effector.Name {
+            table.Add(effector.Name, effector.Description, strings.Join(parameters, ","))
+        }
 	}
 	table.Print()
 }
