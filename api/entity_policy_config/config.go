51ab5533722f1ca97082fd7f871e71bb770845b2
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2015-12-14 16:37:38 +0000

    Add error handling for entity policy config.

--- api/entity_policy_config/config.go
+++ api/entity_policy_config/config.go
@@ -7,39 +7,27 @@ import (
 	"net/url"
 )
 
-func CurrentState(network *net.Network, application, entity, policy string) string {
+func CurrentState(network *net.Network, application, entity, policy string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/current-state", application, entity, policy)
 	body, err := network.SendGetRequest(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(body)
+	return string(body), err
 }
 
-func GetConfigValue(network *net.Network, application, entity, policy, config string) string {
+func GetConfigValue(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/%s", application, entity, policy, config)
 	body, err := network.SendGetRequest(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(body)
+	return string(body), err
 }
 
 // WIP
-func SetConfigValue(network *net.Network, application, entity, policy, config string) string {
+func SetConfigValue(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/%s", application, entity, policy, config)
 	body, err := network.SendEmptyPostRequest(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(body)
+	return string(body), err
 }
 
-func GetAllConfigValues(network *net.Network, application, entity, policy, config string) string {
+func GetAllConfigValues(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config", application, entity, policy, config)
 	body, err := network.SendGetRequest(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(body)
+	return string(body), err
 }
