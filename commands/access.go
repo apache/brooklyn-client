65ae0ded9a1a44090a9ae9e8946456da2b253d32
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2015-12-14 12:27:05 +0000

    Add error handling to access command.

--- commands/access.go
+++ commands/access.go
@@ -7,6 +7,7 @@ import (
 	"github.com/brooklyncentral/brooklyn-cli/command_metadata"
 	"github.com/brooklyncentral/brooklyn-cli/net"
 	"github.com/brooklyncentral/brooklyn-cli/scope"
+    "github.com/brooklyncentral/brooklyn-cli/error_handler"
 )
 
 type Access struct {
@@ -29,6 +30,9 @@ func (cmd *Access) Metadata() command_metadata.CommandMetadata {
 }
 
 func (cmd *Access) Run(scope scope.Scope, c *cli.Context) {
-	access := access_control.Access(cmd.network)
+	access, err := access_control.Access(cmd.network)
+    if nil != err {
+        error_handler.ErrorExit(err)
+    }
 	fmt.Println("Location Provisioning Allowed:", access.LocationProvisioningAllowed)
 }
