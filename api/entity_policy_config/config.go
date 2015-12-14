e578e89594315cae9ecf4edbae47f05b71c6dc36
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2015-12-14 16:51:32 +0000

    Add error handling for sensors and correct return values for entity config.

--- api/entity_policy_config/config.go
+++ api/entity_policy_config/config.go
@@ -10,24 +10,36 @@ import (
 func CurrentState(network *net.Network, application, entity, policy string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/current-state", application, entity, policy)
 	body, err := network.SendGetRequest(url)
-	return string(body), err
+    if nil != err {
+        return "", err
+    }
+	return string(body), nil
 }
 
 func GetConfigValue(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/%s", application, entity, policy, config)
 	body, err := network.SendGetRequest(url)
-	return string(body), err
+    if nil != err {
+        return "", err
+    }
+	return string(body), nil
 }
 
 // WIP
 func SetConfigValue(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config/%s", application, entity, policy, config)
 	body, err := network.SendEmptyPostRequest(url)
-	return string(body), err
+    if nil != err {
+        return "", err
+    }
+	return string(body), nil
 }
 
 func GetAllConfigValues(network *net.Network, application, entity, policy, config string) (string, error) {
 	url := fmt.Sprintf("/v1/applications/%s/entities/%s/policies/%s/config", application, entity, policy, config)
 	body, err := network.SendGetRequest(url)
-	return string(body), err
+    if nil != err {
+        return "", err
+    }
+	return string(body), nil
 }
