ed6eb35ec6cd4a36b16e544a80945973b748eaf5
lloyddave <david@lloyds.org.uk>
2015-12-15 13:59:04 +0000

    Deploy application by reading spec from STDIN
    Use filepath of `-` to indicate reading from STDIN.

--- commands/deploy.go
+++ commands/deploy.go
@@ -7,7 +7,9 @@ import (
 	"github.com/brooklyncentral/brooklyn-cli/command_metadata"
 	"github.com/brooklyncentral/brooklyn-cli/net"
 	"github.com/brooklyncentral/brooklyn-cli/scope"
-    "github.com/brooklyncentral/brooklyn-cli/error_handler"
+	"github.com/brooklyncentral/brooklyn-cli/error_handler"
+	"io/ioutil"
+	"os"
 )
 
 type Deploy struct {
@@ -24,15 +26,30 @@ func (cmd *Deploy) Metadata() command_metadata.CommandMetadata {
 	return command_metadata.CommandMetadata{
 		Name:        "deploy",
 		Description: "Deploy a new brooklyn application from the supplied YAML",
-		Usage:       "BROOKLYN_NAME [ SCOPE ] deploy FILEPATH",
+		Usage:       "BROOKLYN_NAME deploy <FILEPATH|->",
 		Flags:       []cli.Flag{},
 	}
 }
 
 func (cmd *Deploy) Run(scope scope.Scope, c *cli.Context) {
-	create, err := application.Create(cmd.network, c.Args().First())
-    if nil != err {
-        error_handler.ErrorExit(err)
-    }
-	fmt.Println(create)
+	if c.Args().First() == "" {
+		error_handler.ErrorExit("A filename or '-' must be provided as the first argument",error_handler.CLIUsageErrorExitCode)
+	}
+	if c.Args().First() == "-" {
+		blueprint, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			error_handler.ErrorExit(err)
+		}
+		create, err := application.CreateFromBytes(cmd.network, blueprint)
+		if nil != err {
+			error_handler.ErrorExit(err)
+		}
+		fmt.Println(create)
+	} else {
+		create, err := application.Create(cmd.network, c.Args().First())
+		if nil != err {
+			error_handler.ErrorExit(err)
+		}
+		fmt.Println(create)
+	}
 }
