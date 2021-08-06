3c24be22b795a603345c6a5f3ec99fa321daab21
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2021-08-06 20:06:34 +0100

    fix api change

--- java/src/test/java/org/apache/brooklyn/rest/client/ApplicationResourceIntegrationTest.java
+++ java/src/test/java/org/apache/brooklyn/rest/client/ApplicationResourceIntegrationTest.java
@@ -166,7 +166,7 @@ public class ApplicationResourceIntegrationTest {
         Asserts.succeedsEventually(ImmutableMap.of("timeout", timeout), new Runnable() {
             @Override
             public void run() {
-                Object status = api.getSensorApi().get(app, entity, "service.state", false);
+                Object status = api.getSensorApi().get(app, entity, "service.state", false, false);
                 assertTrue(state.toString().equalsIgnoreCase(status.toString()), "status="+status);
             }});
     }
